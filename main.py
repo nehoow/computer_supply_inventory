baab58e71762bfddd90d48c4447086f7517f51ba
nehoow <anykkobaylan@gmail.com>
2025-06-24 12:58:22 +0800

    Added a argparse structure needed for adding item on its  item type and required details of it.

--- main.py
+++ main.py
@@ -19,6 +19,31 @@ def main():
         item_parser.add_argument("--qty", type=int, required=True, help="Initial quantity")
         item_parser.add_argument("--price", type=float, required=True, help="Price per unit")
 
+    # CPU
+    cpu_parser = add_subparsers.add_parser("cpu", help="Add a CPU.")
+    add_common_args(cpu_parser)
+    cpu_parser.add_argument("--socket", required=True, help="CPU Socket Type (e.g., AM5, LGA1700)")
+
+    # RAM
+    ram_parser = add_subparsers.add_parser("ram", help="Add RAM.")
+    add_common_args(ram_parser)
+    ram_parser.add_argument("--capacity_gb", type=int, required=True, help="Capacity in GB")
+
+    # Monitor
+    monitor_parser = add_subparsers.add_parser("monitor", help="Add a Monitor.")
+    add_common_args(monitor_parser)
+    monitor_parser.add_argument("--size_inch", type=float, required=True, help="Screen size in inches")
+
+    # Storage
+    storage_parser = add_subparsers.add_parser("storage", help="Add Storage.")
+    add_common_args(storage_parser)
+    storage_parser.add_argument("--capacity_gb", type=int, required=True, help="Capacity in GB")
+
+    # Peripheral
+    peripheral_parser = add_subparsers.add_parser("peripheral", help="Add a Peripheral (Keyboard, Mouse, etc.).")
+    add_common_args(peripheral_parser)
+    peripheral_parser.add_argument("--dev_type", required=True, help="Device type (e.g., Keyboard, Mouse, Webcam)")
+    
     args = parser.parse_args()
 
     try:
